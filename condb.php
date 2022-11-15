70925e2a91d32ae94c775f9a96644eff6e86554c
Phatcharapongg <116098109+Phatcharapongg@users.noreply.github.com>
2022-11-15 19:07:59 +0700

    Add files via upload

---
+++ condb.php
@@ -0,0 +1,6 @@
+<?php
+$con= mysqli_connect("sql305.epizy.com","epiz_33003692","Qzhhl2e2sjC","epiz_33003692_login_db") or die("Error: " . mysqli_error($con));
+mysqli_query($con, "SET NAMES 'utf8' ");
+error_reporting( error_reporting() & ~E_NOTICE );
+date_default_timezone_set('Asia/Bangkok');
+?>
